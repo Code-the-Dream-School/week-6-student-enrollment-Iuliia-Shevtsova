3a6ad62a5f664d800da7e11c670f811f57f1ffb6
Iuliia Shevtsova <uliyaal2018@gmail.com>
2020-07-19 17:04:43 -0400

    display each student enrolled courses

--- index.js
+++ index.js
@@ -3,197 +3,186 @@ const studentsBut = document.getElementById('students');
 const coursesBut = document.getElementById('courses');
 const new_studentBut = document.getElementById('new_student');
 const saveChangesBut = document.querySelector('.save');
+const saveNewstudentBut = document.querySelector('.saveNewstudentBut');
 const url_students = 'https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Students.json';
 const url_courses = 'https://code-the-dream-school.github.io/JSONStudentsApp.github.io/Courses.json';
+const form =  document.getElementById('formCourses');
+
 
 const divMain = document.createElement('div');
 divMain.className = 'main';
 container.appendChild(divMain);
+//for students's list
 const ul = document.createElement('ul');
 ul.className = 'studentList row';
 divMain.appendChild(ul);
 
-//async function
-async function getJSON2 (url, generateHTML){
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return generateHTML(data);
-  } catch (e) {
-      console.log(e);
-  }
-}
+//selected course in the modal form
+const select = document.getElementById('position');
 
-// an AJAX request
-function getJSON(url, generateHTML) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      console.log(data);
-      return generateHTML(data); // call or return function callback with the data, that got from the Ajax request in JSON format
+//async function
+async function getJSON2 (url){
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (e) {
+        console.log(e);
     }
-  };
-  xhr.send();
-};
+}
 
 //creating list of students
 studentsBut.addEventListener ('click', (e) => {
-  e.preventDefault();
-  console.log(studentsBut.click);
-  var studentHTML = getJSON2(url_students, generateStudentsHTML);
-  var selectHTML = getJSON2(url_courses, generateSelect);
-  console.log(studentHTML);
+    e.preventDefault();
+    Promise.all([
+        getJSON2(url_students),//array of students's info
+        getJSON2(url_courses) //array of courses's info
+    ])
+    .then (data => {
+        var students = data[0];
+        var courses = data[1];
+        console.log(students);
+        console.log(courses);
+        generateStudentsHTML(students);
+        generateSelect(courses);
+    })
 });
 
-//creating list of courses
-coursesBut.addEventListener ('click', (e) => {
-  e.preventDefault();
-var studentHTML = getJSON2(url_courses, generateCoursesHTML);
-});
-
-
-// //save choosed in the list course and display it
-// saveChangesBut.addEventListener('click', () => {
-//   var coursesArray = document.getElementsByTagName('option'); //array of courses in the modal form
-//   console.log(li.className);
-//   var coursebtn = document.querySelector('coursebtn');
-//   // create span inside li item to add choosed course 
-//   const span = document.createElement('span');
-//   for (let i=0; i<coursesArray.length; i++){
-//     if (coursesArray[i].selected) { // if selected course == true
-//       var selectedCourse = coursesArray[i].value;
-//       span.textContent = selectedCourse;
-//     }
-//   }
-//   li.appendChild(span);
+// addCourse.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     saveChangesBut.id = addCourse.id;
 // });
 
 
-//add courses in the form when click the button "Add course"
-function generateSelect(data) {
-  const select = document.getElementById('position');
-  for (let i=0; i<data.length; i++) {
-    var option = document.createElement('option');
-    //var optionValue = document.createAttribute('value');
-    //optionValue.value = i+'_course';
-    //option.setAttributeNode(optionValue);
-    option.textContent = data[i].name;
-    select.appendChild(option);
-  }
+
+function addCourseToStudent(studentID, selectedCourse) {
+    this.students.forEach(student => {
+        if (student.id === studentID) {
+            student.courses.push(selectedCourse);
+        }
+    })
+   
 }
 
-//save choosed in the list course and display it
-function saveChanges(li) {
-  var coursesArray = document.getElementsByTagName('option'); //array of courses in the modal form
-  var coursebtn = document.querySelector('.coursebtn');
-  // create span inside li item to add choosed course 
-  const span = document.createElement('span');
-  
-  saveChangesBut.addEventListener('click', (e) => {
-    e.preventDefault();
-    for (let i=0; i<coursesArray.length; i++){
-      if (coursesArray[i].selected) { // if selected course == true
-        var selectedCourse = coursesArray[i].value;
-        span.textContent = selectedCourse;
-      }
+class Student {
+    constructor(name, last_name, status, id) {
+        this.name = name; 
+        this.last_name = last_name; 
+        this.status = status; 
+        this.id = id;
+        this.courses = [];
     }
-    li.insertBefore(span, coursebtn);
-  });
-};
+}
+
+function getStudentsAray(data){
+    var students = [];
+    data.forEach(element => {
+        var student = new Student(element.name, element.last_name, element.status, element.id, element.courses);
+        students.push(student);
+    })
+    return students;
+}
+
+function getCoursesAray(data){
+    var students = [];
+    data.forEach(element => {
+        var student = new Student(element.name, element.last_name, element.status, element.id);
+        students.push(student);
+    })
+    return students;
+}
+
+function studentsHTML(students) {
+    console.log(students);
+    students.forEach(student => {
+        
+        //create li for each student
+        const li = document.createElement('li');
+
+        // div for putting name and status on one block on the same line
+        const div = document.createElement('div'); 
+        div.className = 'display_status';
+        // create span to display student's name
+        const spanName = document.createElement('span'); //student's name
+        spanName.textContent = student.name +' '+ student.last_name;
+        div.appendChild(spanName);
+        //creating circle for displaying student's status
+        if (student.status) { 
+        const spanStatus = document.createElement('span'); //student's status
+        spanStatus.className = 'status';
+        div.appendChild(spanStatus);}
+        li.appendChild(div);
+
+        //create span for displaying array of student's courses
+        student.courses.forEach(course => {
+            var spanCourse = document.createElement('span'); //array of student's courses
+            spanCourse.textContent = course;
+            console.log(course);
+            li.appendChild(spanCourse);
+        })
+
+        //create 'Add course' button atributes
+        var addCourse = document.createElement('button');
+        addCourse.className = ('btn btn-outline-info coursebtn');
+        //addCourse.id = student.id;
+        addCourse.textContent = 'Add course';
+        li.appendChild(addCourse);
+
+        //create form elements for choosing courses
+        var toggleAtt = document.createAttribute("data-toggle");       // Create a "data-toggle" attribute
+        toggleAtt.value = "modal";                           // Set the value of the "data-toggle" attribute
+        addCourse.setAttributeNode(toggleAtt);                          // Add the class attribute to addCourse button
+
+        var targetAtt = document.createAttribute("data-target");       // Create a "data-target" attribute
+        targetAtt.value = "#courses_id";                           // Set the value of the "data-target" attribute
+        addCourse.setAttributeNode(targetAtt);                          // Add the class attribute to addCourse button
+        
+        //create Edit button
+        const editButton = document.createElement('button');
+        editButton.className = ('btn btn-outline-info');
+        editButton.textContent = 'Edit info';
+        li.appendChild(editButton);
+
+        ul.appendChild(li); 
+
+        addCourse.addEventListener('click', (e) => {
+            e.preventDefault();
+            saveChangesBut.id = student.id;
+        });
+    });
+}
 
 //creating HTML for students info
 function generateStudentsHTML(data) {
-  ul.textContent = '';
-  for (let i=0; i<data.length; i++) {
-    //create li for each student
-    const li = document.createElement('li');
-    li.className = data[i].id+'_student'; // li class of each student =id_number of student
-    const div = document.createElement('div'); // div for putting name and status on one block on the same line
-    div.className = 'display_status';
-    // create span inside li item to easier acsess
-    const spanName = document.createElement('span'); //student's name
-    spanName.textContent = data[i].name +' '+data[i].last_name;
-    div.appendChild(spanName);
-    //creating circle for displaying student's status
-    if (data[i].status) { 
-      const spanStatus = document.createElement('span'); //student's status
-      spanStatus.className = 'status';
-    div.appendChild(spanStatus);}
-    li.appendChild(div);
+    ul.textContent = '';
+    var students = getStudentsAray(data);
+    console.log(students);
+    studentsHTML(students);
     
-    //create 'Add course' button using js
-    const addCourse = document.createElement('button');
-    addCourse.className = ('btn btn-outline-info coursebtn');
-    addCourse.textContent = 'Add course';
-    li.appendChild(addCourse);
-
-    //create form element for choosing courses
-    var toggleAtt = document.createAttribute("data-toggle");       // Create a "data-toggle" attribute
-    toggleAtt.value = "modal";                           // Set the value of the "data-toggle" attribute
-    addCourse.setAttributeNode(toggleAtt);                          // Add the class attribute to addCourse button
-
-    var targetAtt = document.createAttribute("data-target");       // Create a "data-target" attribute
-    targetAtt.value = "#courses_id";                           // Set the value of the "data-target" attribute
-    addCourse.setAttributeNode(targetAtt);                          // Add the class attribute to addCourse button
-    
-    //create Edit button using js
-    const editButton = document.createElement('button');
-    editButton.className = ('btn btn-outline-info');
-    editButton.textContent = 'Edit info';
-    li.appendChild(editButton);
-
-    ul.appendChild(li);  
-
-    addCourse.addEventListener ('click', (e) => {
-      e.preventDefault();
-      saveChanges(li);
+    saveChangesBut.addEventListener('click', (e) => {
+        console.log('button clicked');
+        e.preventDefault();
+        var buttonID = saveChangesBut.id;        
+        var selectedCourse = select.value;
+        students.forEach(student => {
+            if (student.id == buttonID && student.courses.length<4) {
+                student.courses.push(selectedCourse);
+                console.log(student.courses);
+            }
+        })
+        ul.textContent = '';
+        studentsHTML(students);
     });
-      
-  }
 }
 
-//creating HTML for courses info
-function generateCoursesHTML(data) {
-  ul.textContent = '';
-  for (let i=0; i<data.length; i++) {
-    //create li for each student
-    const li = document.createElement('li');
-    li.className = data[i].id+'_course'; // li class of each student =id_number of student
-    // create span inside li item to easier acsess
-    const div = document.createElement('div'); // div for putting name and status on one block on the same line
-    div.className = 'display_hours';
-    const spanCourse = document.createElement('span'); //course's name
-    spanCourse.textContent = data[i].name;
-    div.appendChild(spanCourse);
-    //creating circle for displaying course's hours
-    const spanHourse = document.createElement('span');
-    spanHourse.className = 'hours';
-    spanHourse.textContent = data[i].duration;// quantity of hours of each course
-    div.appendChild(spanHourse);
-    li.appendChild(div);
-    //create 'Add student' button using js
-    const addStudent = document.createElement('button');
-    addStudent.className = ('btn btn-outline-info');
-    addStudent.textContent = 'Add student';
-    li.appendChild(addStudent);
-
-    ul.appendChild(li);  
-  }
+//generate option tags for choosing courses
+function generateSelect(courses) {
+    for (let i=0; i<courses.length; i++) {
+      var option = document.createElement('option');
+      option.textContent = courses[i].name;
+      select.appendChild(option);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
